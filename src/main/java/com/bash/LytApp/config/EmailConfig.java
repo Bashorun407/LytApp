d8e1353b4ef4929395255124a63de6f1f772155f
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2025-11-28 10:54:21 +0100

    Updated EmailConfig and verification-email.html files to send gmail to registered users worked

--- src/main/java/com/bash/LytApp/config/EmailConfig.java
+++ src/main/java/com/bash/LytApp/config/EmailConfig.java
@@ -16,8 +16,8 @@ public class EmailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("your-email@gmail.com");
-        mailSender.setPassword("your-app-password");
+        mailSender.setUsername("lytwind25@gmail.com");
+        mailSender.setPassword("dsallllfnucsozkm");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
@@ -27,4 +27,21 @@ public class EmailConfig {
 
         return mailSender;
     }
+
+//    @Bean
+//    public JavaMailSender javaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("mail.privateemail.com");
+//        mailSender.setPort(587);
+//        mailSender.setUsername("app@lytxtreme.online");
+//        mailSender.setPassword("Bash@2025*#");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true"); // Set to false in production
+//
+//        return mailSender;
+//    }
 }
