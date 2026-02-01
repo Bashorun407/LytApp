4dd7571890570a1deaae5a0adf2b16cd07509f94
Oluwaseyi Bashorun407 <boboladvat@gmail.com>
2026-02-01 15:38:42 +0100

    Commented out redundant DTOs (UserPrincipalDto, VerificationRequestDto, AuthRequestDto)

--- src/main/java/com/bash/LytApp/dto/AuthRequestDto.java
+++ src/main/java/com/bash/LytApp/dto/AuthRequestDto.java
@@ -1,15 +1,15 @@
-package com.bash.LytApp.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record AuthRequestDto(
-
-        @NotBlank(message = "Email is required")
-        @Email(message = "Email should be valid")
-        String email,
-
-        @NotBlank(message = "Password is required")
-        String password
-) {
-}
+//package com.bash.LytApp.dto;
+//
+//import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.NotBlank;
+//
+//public record AuthRequestDto(
+//
+//        @NotBlank(message = "Email is required")
+//        @Email(message = "Email should be valid")
+//        String email,
+//
+//        @NotBlank(message = "Password is required")
+//        String password
+//) {
+//}
--- src/main/java/com/bash/LytApp/dto/UserPrincipalDto.java
+++ src/main/java/com/bash/LytApp/dto/UserPrincipalDto.java
@@ -1,12 +1,12 @@
-package com.bash.LytApp.dto;
-
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
-
-public record UserPrincipalDto(
-        Long id,
-        String email,
-        Collection<? extends GrantedAuthority> authorities
-) {
-}
+//package com.bash.LytApp.dto;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//
+//import java.util.Collection;
+//
+//public record UserPrincipalDto(
+//        Long id,
+//        String email,
+//        Collection<? extends GrantedAuthority> authorities
+//) {
+//}
